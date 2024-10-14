01ac38d616395328fd64608e3729f7e155c86a24
IoritzPerugorria <ioritzperugorria@gmail.com>
2024-10-14 12:41:38 +0200

    Primera implementacion de colecionables, aun hay trabajo

---
+++ Assets/Scripts/Coin.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class Coin : MonoBehaviour
+{
+    public int numero;
+    void OnTriggerEnter2D(Collider2D collider2D){
+        if (collider2D.tag == "Player"){
+            GameObject.Find("Level").GetComponent<Level>().colectCoin(1);
+            Destroy(gameObject);
+        }
+    }
+}
--- Assets/Scripts/Level.cs
+++ Assets/Scripts/Level.cs
@@ -1,8 +1,11 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using Microsoft.Unity.VisualStudio.Editor;
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
+using Image = UnityEngine.UI.Image;
 
 public class Level : MonoBehaviour
 {
@@ -13,14 +16,14 @@ public class Level : MonoBehaviour
     int minutos;
     int segundos;
     int decimas;
-    
+
+    public Image moneda1;
 
     void Start()
     {
         
     }
 
-   
     void Update()
     {
         timer += Time.deltaTime;
@@ -30,5 +33,14 @@ public class Level : MonoBehaviour
         decimas = (int) ((timer - (int)timer) * 100f);
 
         timerUI.text = String.Format("{0:00}:{1:00}:{2:00}", minutos, segundos, decimas);
+    }
+
+    public void colectCoin(int number){
+        switch(number){
+            case 1:
+                moneda1.enabled = false;
+                break;
+
+        }
     }
 }
