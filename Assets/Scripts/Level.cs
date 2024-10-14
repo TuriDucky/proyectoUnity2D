198011df3c309c7339dc7302bd653aa1f0b2db1b
IoritzPerugorria <ioritzperugorria@gmail.com>
2024-10-14 11:54:40 +0200

    Se ha añadido un temporizador de nivel.

---
+++ Assets/Scripts/Level.cs
@@ -0,0 +1,34 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class Level : MonoBehaviour
+{
+
+    public TMP_Text timerUI;
+    float timer;
+
+    int minutos;
+    int segundos;
+    int decimas;
+    
+
+    void Start()
+    {
+        
+    }
+
+   
+    void Update()
+    {
+        timer += Time.deltaTime;
+
+        minutos = (int) (timer / 60f);
+        segundos = (int) (timer - minutos * 60f);
+        decimas = (int) ((timer - (int)timer) * 100f);
+
+        timerUI.text = String.Format("{0:00}:{1:00}:{2:00}", minutos, segundos, decimas);
+    }
+}
