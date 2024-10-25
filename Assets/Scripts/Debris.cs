c60a10117cd34c55d3421973bd1f4df1fd8c9644
3MASANZ-A17-08\AlumTA <iperugoald1@educacion.navarra.es>
2024-10-25 21:02:13 +0200

    Unos graficos nuevos

--- Assets/Scripts/Debris.cs
+++ Assets/Scripts/Debris.cs
@@ -5,23 +5,12 @@ using UnityEngine;
 public class Debris : MonoBehaviour
 {
     public int numParticles;
+    public int direccionEscombros;
 
     public GameObject particle1;
     public GameObject particle2;
     public GameObject particle3;
     public GameObject particle4;
-    
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 
     void OnTriggerEnter2D(Collider2D collision2D){
         
@@ -44,18 +33,22 @@ public class Debris : MonoBehaviour
                 case 0:
                     GameObject laParticula = Instantiate(particle1, posicion, Quaternion.identity);
                     laParticula.layer = 6;
+                    laParticula.GetComponent<Derbis_Particles>().setLockX(direccionEscombros);
                     break;
                 case 1:
                     GameObject laParicula2 = Instantiate(particle2, posicion, Quaternion.identity);
                     laParicula2.layer = 6;
+                    laParicula2.GetComponent<Derbis_Particles>().setLockX(direccionEscombros);
                     break;
                 case 2:
                     GameObject laParicula3 = Instantiate(particle3, posicion, Quaternion.identity);
                     laParicula3.layer = 6;
+                    laParicula3.GetComponent<Derbis_Particles>().setLockX(direccionEscombros);
                     break;
                 case 3:
                     GameObject laParicula4= Instantiate(particle4, posicion, Quaternion.identity);
                     laParicula4.layer = 6;
+                    laParicula4.GetComponent<Derbis_Particles>().setLockX(direccionEscombros);
                     break;
             }
         }
--- Assets/Scripts/Derbis_Particles.cs
+++ Assets/Scripts/Derbis_Particles.cs
@@ -6,7 +6,7 @@ public class Derbis_Particles : MonoBehaviour
 {
     Rigidbody2D rb2D;
     float timer;
-    public float lockX; // 0 = fuerza aleatoria, 1 = fuerza a la derecha, 2 = fuerza a la izquierda, 3 = no hay fuerza horizontal
+    int lockX; // 0 = fuerza horizontal aleatoria, 1 = fuerza a la derecha, 2 = fuerza a la izquierda, 3 = fuerza hacia abajo, 4 = no hay fuerza 
     public float maxForce;
     void Start()
     {
@@ -25,6 +25,9 @@ public class Derbis_Particles : MonoBehaviour
             case 2:
                 vectorx = Random.Range(-maxForce, 0);
                 break;
+            case 3:
+                vectory = Random.Range(-maxForce, 0);
+                break;
         }
         rb2D.velocity = new Vector2(vectorx, vectory);
         rb2D.angularVelocity = Random.Range(-500,500);
@@ -35,6 +38,10 @@ public class Derbis_Particles : MonoBehaviour
         if (timer <= 0){
             Destroy(gameObject);
         }
+    }
+
+    public void setLockX(int direccion){
+        lockX = direccion;
     }
 
     
